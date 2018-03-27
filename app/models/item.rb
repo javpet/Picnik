7e8e5fec62c4ba67866da17cde270261250110fb
javpet <javorkai.peter@gmail.com>
2018-03-28 00:18:35 +0200

    Added category_items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,9 @@
 class Item < ApplicationRecord
 
+  has_many :category_items
+  has_many :categories, through: :category_items
+  # Category items model basically sits in between Item and Category models to make the connection
+
   validates :title, presence: true;
 
   mount_uploader :image, ImageUploader
